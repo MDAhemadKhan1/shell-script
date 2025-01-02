2161a72621fabb2fbd79e7d191b83d2cfa2dff5b
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-02 11:22:31 +0530

    conversation4

---
+++ 05-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERNAME=$1
+read $USERNAME
+
+echo "please enter user name ::"
+echo "entered username is ::"
+
+
+PASSWORD=$2
+read $PASSWORD
+
+echo "please enter password ::"
+echo "entered password is ::"
