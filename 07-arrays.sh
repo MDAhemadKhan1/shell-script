65b4550fb1834d9363ff14c668ada492138fd235
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-11 11:24:24 +0530

    arrays

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -6,10 +6,10 @@ echo ${Movies[1]}
 echo ${Movies[2]}
 
 echo ${Movies[@]}
-echo $0
-echo $PWD 
-echo $HOME
+#echo $0
+#echo $PWD 
+#echo $HOME
 echo $USER 
-echo $$
-sleep 60 &
-echo $!
+#echo $$
+#sleep 60 &
+#echo $!
