4ab4f65e9d259fdf1aeb1acbfde989340c9a6f54
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-11 11:57:08 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-echo "enter the number"
-read $number
+NUMBER=$1
 
-if [ $number -lt 10 ]
+# -gt, -lt, -eq, -ge, -le
 
+if [ $NUMBER -gt 100 ]
 then
-
-    echo "given number is less than 10"
-
+    echo "Given number is greater than 100"
 else
-
-    echo "given number is greater than 10"
-
+    echo "Given number is less than or equal to 100"
 fi
