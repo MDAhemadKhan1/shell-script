38cc625f2739a4ce5e5f80b911b60ca87b1d7a01
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-11 14:32:15 +0530

    install

--- 11-git-install.sh
+++ 11-git-install.sh
@@ -2,17 +2,17 @@
 
 UserId=$(id -u)
 
-if [$UserId -ne 0]
+if [ $UserId -ne 0 ]
 then 
     echo "ERROR :: you must have user access to run this script"
     exit 1
 fi
 
 dnf list installed git
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then
     dnf install git -y
-    if [$? -ne 0]
+    if [ $? -ne 0 ]
     then
         echo "Git Installation is ...........failed"
         exit 1
