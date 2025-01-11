3db80c2ce6e55adec4c8b11cd57316b0e1e9a69e
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-11 11:40:09 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-number=$1
-if[$1 -gt 10 ]
-then
-echo "given number is greater than 10"
-else
-echo "given number is less than 10"
+NUMBER=$1
 
+# -gt, -lt, -eq, -ge, -le
+
+if [ $NUMBER -gt 100 ]
+then
+    echo "Given number is greater than 100"
+else
+    echo "Given number is less than or equal to 100"
 fi
