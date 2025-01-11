3ca437e547261a7b1f8cc0f6f1d837ebb0e360d2
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-11 13:22:23 +0530

    install

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -4,29 +4,21 @@
 USERID=$(id -u)
 
 #if the user is root user id will be equal to zero(0)
-if [$USERID -ne 0]
-then
-    echo "ERROR:: user is not the root user"
-    exit 1
+if [$USERID -ne 0]; then
+     echo "ERROR:: user is not the root user"
+     exit 1
 fi
 
 dnf list installed mysql
-if [$? -ne 0]
-then
+if [$? -ne 0]; then
      dnf install mysql -y
-     if [$? -ne 0]
-     then 
+     if [$? -ne 0]; then
           echo "installation is....... failure"
-    
-     else 
+
+     else
           echo "installtion is........ success"
      fi
-else 
+else
      echo "mysql is already installed"
 fi
 
-
-
-
- 
-       
