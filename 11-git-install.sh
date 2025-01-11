9002282b299f65bed64fc243ed7bef99ea8a72cb
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-11 14:29:45 +0530

    install

---
+++ 11-git-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+UserId=$(id -u)
+
+if [$UserId -ne 0]
+then 
+    echo "ERROR :: you must have user access to run this script"
+    exit 1
+fi
+
+dnf list installed git
+if [$? -ne 0]
+then
+    dnf install git -y
+    if [$? -ne 0]
+    then
+        echo "Git Installation is ...........failed"
+        exit 1
+    else
+        echo "Git Installation is ...........Success"
+    fi    
+else
+    echo "git already exist"
+fi
+
+
