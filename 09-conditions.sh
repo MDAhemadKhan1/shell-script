922eabc006e30617f69ce4175d9fb5c928d22fdc
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-11 11:54:04 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -7,10 +7,10 @@ if [ $number  < 10 ]
 
 then
 
-    echo "given number is greater than 10"
+    echo "given number is less than 10"
 
 else
 
-    echo "given number is less than 10"
+    echo "given number is greater than 10"
 
 fi
