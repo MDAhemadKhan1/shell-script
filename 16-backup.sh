26b110c73da2b091b25801bbd212bcf4cabfa83a
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-13 18:01:21 +0530

    logs

--- 16-backup.sh
+++ 16-backup.sh
@@ -34,7 +34,7 @@ if [ -n "$Files" ]; then
     echo "deleting the file "
     rm -rf $file
         echo "deleted the file "
-
+   done <<< $Zip_File
    else
    echo " failed to zip the file"
    fi
