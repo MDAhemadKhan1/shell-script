e7f41923491e161f9bee15551b32f52caea03957
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-14 16:21:00 +0530

    logs

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -2,14 +2,18 @@
 
 Disk_Usage=$(df -hT | grep xfs)
 Disk_Threshold=5
+MSG=""
 
-while read -r line
-do 
-  echo "usage is $line"
-  Usage=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1 )
-  Partition=$(echo $line | awk -F " " '{print $7f}')
+while read -r line; do
+    echo "xfs files are $line"
+    Usage=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
+    echo "usage is $Usage"
+    Partition=$(echo $line | awk -F " " '{print $7f}')
+    echo "partition is $Partition"
+    if [ $Usage -gt 5 ]; then
+        echo "usage is:: $Usage for :: $Partition"
+    fi
 
-  
-done <<< $Disk_Usage
-
-
+done <<<$Disk_Usage
+MSG+= "Hiiiii .......this is mail regardig the disk usage.....
+Disk usage $Usage for the $Partition"
