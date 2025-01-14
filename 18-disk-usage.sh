0042bd392380257aefaaff5b835245a198dd23e4
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-14 16:43:33 +0530

    logs

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -7,9 +7,11 @@ MSG=""
 while read -r line; do
     Usage=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
     Partition=$(echo $line | awk -F " " '{print $7f}')
-        if [ $Usage -ge $Disk_Threshold ]; then
+        if [ $Usage > $Disk_Threshold ]; then
                 MSG+="for partition :: $Partition the Usage is :: $Usage \n "
         fi
 
 done <<< $Disk_Usage
 echo -e "msg is :: $MSG"
+
+
