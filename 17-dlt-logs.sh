224e39e650c7bd6c912527edf74ff73a3a670bff
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-14 11:40:22 +0530

    logs

---
+++ 17-dlt-logs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "you must have root access to execute this script"
+    exit 1
+fi
+
+SOURCE_DIR="/var/log/applog"
+
+Files=$(find $SOURCE_DIR -name "*.log" -mtime 14)
+
+if [ -f $Files ]; then
+
+    echo "files are .....$Files"
+
+    while read -r file; do
+        echo "files to delete....$file"
+        echo "deleting the file....$file"
+        rm -rf $file
+        echo "deleted the file....$file"
+
+    done <<<$Files
+    exit 1
+else
+    echo "here no files older than 14 days"
+fi
