c77c32f84fbe39756d57bce74d2f4ac96734ab9e
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-01-14 16:35:19 +0530

    logs

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -5,14 +5,12 @@ Disk_Threshold=5
 MSG=""
 
 while read -r line; do
-    echo "xfs files are $line"
     Usage=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
-    
     Partition=$(echo $line | awk -F " " '{print $7f}')
-    
-    if [ $Usage -ge $Disk_Threshold ]; then
-        MSG+="Hiiiii .......this is mail regardig the disk usage.....
-Disk usage $Usage for the $Partition"
-    fi
+    echo "partition is :: $Partition and Usage is :: $Usage"
+    #     if [ $Usage -ge $Disk_Threshold ]; then
+    #         MSG+="Hiiiii .......this is mail regardig the disk usage.....
+    # Disk usage $Usage for the $Partition"
+    #     fi
 
 done <<<$Disk_Usage
